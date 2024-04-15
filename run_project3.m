a0f73a0ca7a9c500649a00c046d0bfe9c96df7f2
Anthony <105174899+Youtubbs@users.noreply.github.com>
2024-04-15 08:38:51 -0500

    Update run_project3.m

--- run_project3.m
+++ run_project3.m
@@ -18,25 +18,25 @@ function run_project3
 
     sens_parameters = [beta, gamma, delta, epsilon, q, v, S0, I0, R0, D0, tspan];
 
-    % % No quarantine, no vaccination model
-    % [t, Y] = ode45(@(t, Y) modelNoQuarantineNoVaccination(t, Y, beta, gamma, delta, epsilon), tspan, [S0; I0; R0; D0]);
-    % stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'No Quarantine, No Vaccination');
-    % displaySensitivities('NQNV', beta, gamma, delta, epsilon, q, v);
-    % 
-    % % Quarantine, no vaccination model
-    % [t, Y] = ode45(@(t, Y) modelQuarantineNoVaccination(t, Y, beta, gamma, delta, epsilon, q), tspan, [S0; I0; R0; D0]);
-    % stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'Quarantine, No Vaccination');
-    % displaySensitivities('QNV', beta, gamma, delta, epsilon, q, v);
-    % 
-    % % No quarantine, with vaccination model
-    % [t, Y] = ode45(@(t, Y) modelNoQuarantineVaccination(t, Y, beta, gamma, delta, epsilon, v), tspan, [S0; I0; R0; D0]);
-    % stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'No Quarantine, With Vaccination');
-    % displaySensitivities('NQV', beta, gamma, delta, epsilon, q, v);
-    % 
-    % % Quarantine and vaccination model
-    % [t, Y] = ode45(@(t, Y) modelQuarantineVaccination(t, Y, beta, gamma, delta, epsilon, q, v), tspan, [S0; I0; R0; D0]);
-    % stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'Quarantine and Vaccination');
-    % displaySensitivities('QV', beta, gamma, delta, epsilon, q, v);
+    % No quarantine, no vaccination model
+    [t, Y] = ode45(@(t, Y) modelNoQuarantineNoVaccination(t, Y, beta, gamma, delta, epsilon), tspan, [S0; I0; R0; D0]);
+    stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'No Quarantine, No Vaccination');
+    displaySensitivities('NQNV', beta, gamma, delta, epsilon, q, v);
+     
+    % Quarantine, no vaccination model
+    [t, Y] = ode45(@(t, Y) modelQuarantineNoVaccination(t, Y, beta, gamma, delta, epsilon, q), tspan, [S0; I0; R0; D0]);
+    stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'Quarantine, No Vaccination');
+    displaySensitivities('QNV', beta, gamma, delta, epsilon, q, v);
+     
+    % No quarantine, with vaccination model
+    [t, Y] = ode45(@(t, Y) modelNoQuarantineVaccination(t, Y, beta, gamma, delta, epsilon, v), tspan, [S0; I0; R0; D0]);
+    stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'No Quarantine, With Vaccination');
+    displaySensitivities('NQV', beta, gamma, delta, epsilon, q, v);
+     
+    % Quarantine and vaccination model
+    [t, Y] = ode45(@(t, Y) modelQuarantineVaccination(t, Y, beta, gamma, delta, epsilon, q, v), tspan, [S0; I0; R0; D0]);
+    stackedPlots(t, Y', {'Susceptible', 'Infected', 'Recovered', 'Deceased'}, 'Quarantine and Vaccination');
+    displaySensitivities('QV', beta, gamma, delta, epsilon, q, v);
 
     run_sensitivity_analysis(sens_parameters)
 
